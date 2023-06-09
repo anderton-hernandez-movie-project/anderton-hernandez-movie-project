f71a62ab6fbd148f12a22506f713128cbf4687cf
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-06-09 09:52:31 -0700

    Refactored js functions for readability. edited html for layout

--- js/movie.js
+++ js/movie.js
@@ -1,43 +1,49 @@
 
 $(document).ready(function(){
+    `use strict`
+
     let movieArray = [];
     let url = "https://mahogany-dynamic-lip.glitch.me/movies";
+
     const moviePosters = () => {
         let loader = `<div class="loading posters"><img src="img/loading.gif"></div>`;
         $("#container").html(loader);
         fetch(url)
             .then(resp => resp.json())
             .then(movies => {
-                console.log(movies)
-                movieArray = movies;
-                let htmlStr = "";
-                let html = "";
                 let i = 0;
                 for (let movie of movies) {
-
-                    //creates the dropdown menus for select
-                    html += `<option value=${movie.id}>${movie.title}</option>`;
-
-                    //creates movie posters
-                    htmlStr += `<div class="posters grow gradient-border"><div>`
-                    htmlStr += `<h1 class="title">${movie.title}</h1><div class="genre">${movie.genre}</div><div id="image-container${i}"></div>`;
-                    htmlStr += `<div class="underImgContainer"><div class="rating">${createStars(movie)}</div><div class="director">By: ${movie.director}</div></div>`;
-                    htmlStr += `<div class="description">${movie.plot}</div>`;
-                    htmlStr += `</div></div>`;
-                    getPoster(movie.title, i++)
+                    renderMovies(movie, i);
+                    getPoster(movie.title, i++);
                 }
-
-                //pushes created card or dropdown menu to the screen
-                console.log(movies)
-                $("#container").html(htmlStr);
-                $("#selectMenu").html("<option value='-1' selected>Select a movie</option>" + html);
-                $("#selectMenu2").html("<option value='-1' selected>Select a movie</option>" + html);
+                $(`.loading`).remove()
             });
     }
+
     moviePosters();
+
+    //============Renders HtML from fetch===========\\
+    const renderMovies = (movie, iterator) => {
+        //creates the dropdown menus for select
+        html = `<option value=${movie.id}>${movie.title}</option>`;
+
+        //creates movie posters
+        htmlStr = `<div class="posters grow gradient-border"><div>`
+        htmlStr += `<h1 class="title">${movie.title}</h1><div class="genre">${movie.genre}</div><div id="image-container${iterator}"></div>`;
+        htmlStr += `<div class="underImgContainer"><div class="rating">${createStars(movie)}</div><div class="director">By: ${movie.director}</div></div>`;
+        htmlStr += `<div class="description">${movie.plot}</div>`;
+        htmlStr += `</div></div>`;
+        //pushes created card or dropdown menu to the screen
+        $("#container").append(htmlStr);
+        $("#selectMenu").append("<option value='-1' selected>Select a movie</option>" + html);
+        $("#selectMenu2").append("<option value='-1' selected>Select a movie</option>" + html);
+    }
 
     //========Show the edit menu========\\
     $("#showEdit").click(function() {
+        isOpen($("#selectMenu2"))
+        isOpen($("#delete-movie"))
+        isOpen($("#postMovie"))
         $("#editMovie").toggleClass("hidden1");
         $("#selectMenu").toggleClass("hidden1");
     });
@@ -49,6 +55,9 @@ $(document).ready(function(){
 
     //========show the delete menu========\\
     $(".remove-hidden").click(function() {
+        isOpen($("#editMovie"))
+        isOpen($("#selectMenu"))
+        isOpen($("#postMovie"))
         $("#selectMenu2").toggleClass("hidden1");
         $("#delete-movie").toggleClass("hidden1");
     });
@@ -60,12 +69,22 @@ $(document).ready(function(){
 
     //========show the post menu========\\
     $("#post-id").click(function () {
+        isOpen($("#editMovie"))
+        isOpen($("#selectMenu"))
+        isOpen($("#selectMenu2"))
+        isOpen($("#delete-movie"))
         $("#postMovie").toggleClass("hidden1");
     });
     //hide post menu
     $("#newMovie").click(function() {
         $("#postMovie").toggleClass("hidden1");
     })
+    //=========Close menu if open==========\\
+    const isOpen = $id => {
+        if (!$id.hasClass(`hidden1`)) {
+            $id.toggleClass("hidden1");
+        }
+    }
 
     //when the option selected is changed, update the input fields
     $("#selectMenu").change(function(){
@@ -84,6 +103,7 @@ $(document).ready(function(){
         }
     })
 
+    //=========Creates stars from rating========\\
     function createStars(movie){
         let html = "";
         for(var i = 0; i < movie.rating; i++){
@@ -162,45 +182,40 @@ $(document).ready(function(){
             .then(moviePosters).catch(error => console.log(error))
     });
 
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDdiYjg3NzZjMGRkMDQwZjRlNDgyZWI2NTczNTQ4NCIsInN1YiI6IjY0ODIyNWM1NjQ3NjU0MDEwNWMwNTkyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.siL7ZDqKl5__RHhDDo0Z418nSgWn4N1VlpQx2TnHVcY'
-        }
-    };
 
+    //======Gets data from TMDb to create poster image======\\
     const getPoster = (movie, iterator) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: MPK
+            }
+        };
         fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`, options)
             .then(response => response.json())
             .then(response => {
-                console.log(response)
                 response.results.every(function (item) {
-                    let dBTitle = item.title.toLowerCase();
-                    let mTitle = movie.toLowerCase();
-                    console.log(mTitle, dBTitle)
-                    if (mTitle === dBTitle && item.poster_path !== null) {
-                        console.log(item.title)
-                        console.log(`here`)
-                        let posterPath = item.poster_path
-                        let url = `https://image.tmdb.org/t/p/w500${posterPath}`
-                        console.log(`here`)
-                        var img = $('<img/>', {
-                            src: url,
-                            alt: 'Movie poster',
-                            class: 'movie-posters'
-                        })
-                        img.appendTo($(`#image-container${iterator}`));
-                        return false;
-                    }
+                    renderImg(item, movie, iterator)
                 })
             })
-                        .catch(err => console.error(err));
+            .catch(err => console.error(err));
 }
 
-    // fetch(`https://api.themoviedb.org/3/authentication&appid=${MPK}`)
-    //     .then(data => data.json())
-    //     .then(data => console.log(data))
-
-    //end of document ready
+    //=======Renders Image with data from TMDb========\\
+    const renderImg = (item, movie, iterator) => {
+        let dBTitle = item.title.toLowerCase();
+        let mTitle = movie.toLowerCase();
+        if (mTitle === dBTitle && item.poster_path !== null) {
+            let posterPath = item.poster_path
+            let url = `https://image.tmdb.org/t/p/w500${posterPath}`
+            var img = $('<img/>', {
+                src: url,
+                alt: 'Movie poster',
+                class: 'movie-posters'
+            })
+            img.appendTo($(`#image-container${iterator}`));
+            return false;
+        }
+    }
 });
